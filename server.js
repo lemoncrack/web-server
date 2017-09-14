8d735f371a2681b2647ba23e6de5c579bfa64c14
Lemon Crack <lemon.crack17@gmail.com>
2017-09-14 08:43:35 +0500

    update language

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(middleware.logger);
 //app.use(middleware.requireAuthentication); //application level middleware
 //about About Us
 app.get('/about',middleware.requireAuthentication,function(req,res){
-	res.send('About Us!');
+	res.send('About Us!!');
 });
 app.use(express.static(__dirname+'/public'));
 //console.log(__dirname);
